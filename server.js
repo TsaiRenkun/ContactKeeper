e2b6b05f6c99289daa5eea145a9a10f5777c094d
Tsairenkun <Tsairenkun@gmail.com>
2020-06-05 18:44:58 +0800

    Deploy on to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require ('path');
 const app = express();
 
 //conect DataBase
@@ -19,6 +19,14 @@ app.get("/", (req, res) =>
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//Serve STatic Assets in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static Folder
+app.use(express.static('client/build'))
+
+app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
